45802a8173ae40bcc0ff989915435a845de1cfe1
Your Name <you@example.com>
2021-10-13 15:51:50 -0700

    b

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,8 @@ export AIRFLOW_HOME=~/airflow
 export ES_HOME=/home/user/elasticsearch-7.15.0
 export SCRIPT_HOME=/home/user/data_science/scripts
 
+echo "Startin up airflow"
+
 # Airflow
 # some of these initialization steps can be removed once we have a real instance running
 AIRFLOW_VERSION=2.1.4
@@ -15,11 +17,16 @@ airflow users create --username admin --firstname b --lastname b --role Admin --
 airflow webserver --port 8080 &> airflow.log &
 airflow scheduler &> airflow.log &
 
+echo "Airflow running"
+fuser 8080/tcp
+
+echo "Startin up ES"
+
 # Elasticsearch
 $ES_HOME/bin/elasticsearch &> es.log &
 # wait for ES to start
-sleep 5
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 5; done
 # populate with data
-python3 $SCRIPT_HOME/bulk_ingest.py
-# test query
-python3 $SCRIPT_HOME/elasticsearch_test.py
+#python3 $SCRIPT_HOME/bulk_ingest.py
+echo "test query"
+ python3 $SCRIPT_HOME/elasticsearch_test.py
--- scripts/docker/elasticsearch_query.py
+++ scripts/docker/elasticsearch_query.py
@@ -5,17 +5,22 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(['localhost:9200'])
 
+# Doc to add
 doc = {
     'author': 'kimchy',
     'text': 'Elasticsearch: cool. bonsai cool.',
     'timestamp': datetime.now(),
 }
 
+# Add the doc into index
 res = es.index(index='test-index', id=1, document=doc)
 print(res['result'])
 
+# Refresh the index before search
+# https://www.elastic.co/guide/en/elasticsearch/reference/7.15/indices-refresh.html
 es.indices.refresh(index='test-index')
 
+# Execute search for all existing docs in that index
 res = es.search(index='test-index', query={'match_all': {}})
 print('Got %d hits:' % res['hits']['total']['value'])
 [print('%(timestamp)s %(author)s: %(text)s' % hit['_source']) for hit in res['hits']['hits']]
